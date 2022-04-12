bacc53359ad8eda963b663881c92c0d254f0753c
Laxmi Sarki <laxmi.sarki@in.ey.com>
2022-04-13 00:06:22 +0530

    adding comments And final commit

--- app.py
+++ app.py
@@ -1,34 +1,64 @@
 import numpy as np
-from flask import Flask,render_template,jsonify,request
+from flask import Flask, render_template, jsonify, request, flash
 import os
 import joblib
 import pandas as pd
+from constants import *
 
 TEMPLATE_DIR = os.path.abspath('templates')
 STATIC_DIR = os.path.abspath('styles')
-app = Flask(__name__,template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app.secret_key = 'Li-FinB@l@nce'
 
 predictions = dict()
+"""Loading Multi-label Classifier """
 clf = joblib.load('finalized_model.sav')
 
-@app.route('/',methods=["GET"])
+
+@app.route('/', methods=["GET"])
 def create_profile():
     return render_template('create_profile.html')
 
+
 @app.route('/set_goal')
 def set_goal():
-   return render_template('set_goal.html')
+    return render_template('set_goal.html')
+
 
 @app.route('/goal')
 def goal_index():
-   return render_template('goal_index.html')
+    return render_template('goal_index.html')
+
+
+@app.route('/non_eligible_plan')
+def non_eligible_plan():
+    data = {}
+    tax_code = pd.DataFrame.from_dict(predictions, orient="index", columns=["output"])
+    tax_code["tax_section"] = tax_code.index
+    tax_code.index = range(len(tax_code))
+    non_eligible_tax_code = tax_code[tax_code["output"] == 0]
+    for dat in non_eligible_tax_code["tax_section"].values:
+        data.update({dat: tax_section[dat]})
+    return render_template("non_eligibility_card.html", content=data)
+
 
 @app.route('/eligible_plan')
 def eligible_plan():
-   return render_template("eligibility_card.html")
+    data = {}
+    tax_code = pd.DataFrame.from_dict(predictions, orient="index", columns=["output"])
+    tax_code["tax_section"] = tax_code.index
+    tax_code.index = range(len(tax_code))
+    eligible_tax_code = tax_code[tax_code["output"] == 1]
+    for dat in eligible_tax_code["tax_section"].values:
+        data.update({dat: tax_section[dat]})
+    return render_template("eligibility_card.html", content=data)
 
 
-def get_taxable_amount(amount:float)-> float:
+def get_taxable_amount(amount: float) -> float:
+    """
+    :param amount:
+    :return:
+    """
     taxable_amt = 0
     if amount <= 250000:
         taxable_amt = 0
@@ -36,16 +66,12 @@ def get_taxable_amount(amount:float)-> float:
         taxable_amt = (amount - 250000) * 0.05
     elif amount <= 1000000:
         taxable_amt = (amount - 500000) * 0.20 + 12500
-    else :
+    else:
         taxable_amt = (amount - 1000000) * 0.30 + 200000
     return taxable_amt
 
 
-
-
-
-
-@app.route('/predict',methods=["POST"])
+@app.route('/predict', methods=["POST"])
 def classify():
     try:
         amount = float(request.form.get('income'))
@@ -53,14 +79,13 @@ def classify():
         FIRST_PROPERTY = request.form.get('FIRST_PROPERTY')
         SPECIFIC_ILLNESS = request.form.get('SPECIFIC_ILLNESS')
     except Exception as e:
-        return jsonify({"message":"Please Check the Input format Provided"}),400
+        return jsonify({"message": "Please Check the Input format Provided"}), 400
     taxable_amt = get_taxable_amount(amount)
-    data  = np.array([[0,0, 0, 0,0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1]])
+    data = np.array([[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1]])
     pred = clf.predict(data).toarray()
-    pred = pd.DataFrame(pred,columns=['80D', '80DD', '80DDB', '80E', '80EE', '80EEA','80TTB', '80TTA', '80CCD', '80C'])
+    pred = pd.DataFrame(pred,
+                        columns=['80D', '80DD', '80DDB', '80E', '80EE', '80EEA', '80TTB', '80TTA', '80CCD', '80C'])
     global predictions
     predictions = pred.to_dict('list')
-    return render_template('create_profile.html',message ="User Created Successfully")
-
-
-
+    flash("User Created Successfully")
+    return render_template('profile_2_set_goal.html')
---
+++ constants.py
@@ -0,0 +1,36 @@
+""" Below rules are all incorporated into the classifier model"""
+tax_section = {
+   "80D":{
+      "Description":"Deduction for health insurance premium and medical expenses for senior citizens is allowed under this section.Individuals below 60 years can claim up to ₹25,000; whereas senior citizens can claim up to ₹50,000."
+   },
+   "80TTB":{
+      "Description":"This section allows senior citizens (aged 60 years and above) to claim up to 50,000 as a tax deduction from their gross total income."
+   },
+   "80TTA":{
+      "Description":"Income of up to ₹10,000 earned from interest on savings accounts can be claimed as a tax deduction under this section."
+   },
+   "80EEA":{
+      "Description":"This section allows a deduction up to ₹1.5 lakh for interest paid by first-time homebuyers for a loan sanctioned from a financial institution."
+   },
+   "80EE":{
+      "Description":"Deduction is provided on the interest part of the residential house property loan availed from a financial institution.A maximum of 50,000 can be claimed under this section."
+   },
+   "80E":{
+      "Description":"Deduction is provided only on the interest part of the education loan.It is available only for eight years, starting from the year your loan repayment begins or until the interest is fully repaid, whichever is earlier."
+   },
+   "80DD":{
+      "Description":"75,000 for those with 40%-80% disability;₹1.25 lakh for severe disability 80% or more"
+   },
+   "80DDB":{
+      "Description":"Deduction can be claimed for medical treatment of a dependent who is suffering from a specific illness .The amount allowed as deduction is as follows.A maximum of 40,000 or amount paid, whichever is less for individuals under 60 years.A maximum of 1 lakh or amount paid, whichever is less for senior citizens and super senior citizens"
+   },
+   "80CC":{
+      "Description":"Maximum of up to ₹1.5 lakh (aggregate of 80C, 80CCC, and 80CCD)"
+   },
+   "80CCD":{
+      "Description":"Maximum of up to ₹1.5 lakh (aggregate of 80C, 80CCC, and 80CCD)"
+   },
+   "80C":{
+      "Description":"Maximum of up to ₹1.5 lakh (aggregate of 80C, 80CCC, and 80CCD)"
+   }
+}
